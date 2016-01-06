14b537f34f9977a2dbba43dab308cc058f6e6bdf
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-01-07 01:14:37 +0200

    Add comments

--- usr/bin/encrypt-and-mount.sh
+++ usr/bin/encrypt-and-mount.sh
@@ -13,6 +13,7 @@ crypted_devices() {
     dmsetup deps $dev -o blkdevname | awk -NF '\\(|\\)' '{ print $2 }'
   done
 }
+# Check arguments
 if ! [ -b "$1" ]; then
   usage "'$1' not a block device"
 fi
@@ -21,25 +22,35 @@ if ! ([ -d "$2" ] || mkdir -p "$2"); then
 fi
 DEVPATH=$1
 DEV=$(basename $DEVPATH)
+MOUNT_PATH=$2
+# Make sure the block device is not already mapped
 if crypted_devices | grep $DEV > /dev/null; then
   CRYPTDEV=$(crypted_devices | grep $DEV | cut -d" " -f1)
   umount -f /dev/mapper/$CRYPTDEV
   cryptsetup close $CRYPTDEV
 fi
-MOUNT_PATH=$2
-TMPDIR=$(mktemp -d)
-mount tmpfs $TMPDIR -t tmpfs -o size=32m
-dd if=/dev/urandom of=$TMPDIR/disk.pwd bs=512 count=4 status=none iflag=fullblock
-chmod 600 $TMPDIR/disk.pwd
+# Make sure the device is not already directly mounted
+umount -f $DEVPATH
+# Find a free mapping
 COUNT=1
 while [ -e /dev/mapper/e$COUNT ]; do
   COUNT=$(($COUNT + 1))
 done
 CRYPTDEV=e$COUNT
-umount -f $DEVPATH
+# Create a random keyfile
+TMPDIR=$(mktemp -d)
+mount tmpfs $TMPDIR -t tmpfs -o size=32m
+touch $TMPDIR/disk.pwd
+chmod 600 $TMPDIR/disk.pwd
+dd if=/dev/urandom of=$TMPDIR/disk.pwd bs=512 count=4 status=none iflag=fullblock
+#Open plain dm-crypt mapping
 cryptsetup --cipher=aes-xts-plain64 --key-file=$TMPDIR/disk.pwd --key-size=512 \
 open --type=plain $DEVPATH $CRYPTDEV
+# Get rid of keyfile
 umount -f $TMPDIR
+# Make sure the mount path is not already mounted
 umount -f $MOUNT_PATH
+# Create a filesystem
 mkfs.ext4 /dev/mapper/$CRYPTDEV
+# Mount
 mount /dev/mapper/$CRYPTDEV $MOUNT_PATH

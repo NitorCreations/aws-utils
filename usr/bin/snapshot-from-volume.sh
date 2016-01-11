8b1414e5cf6c89af6b1e928a35b59a2f6a13bf7e
Mika Majakorpi <mika@majakorpi.net>
2016-01-11 08:07:48 +0200

    Fix

--- usr/bin/snapshot-from-volume.sh
+++ usr/bin/snapshot-from-volume.sh
@@ -15,7 +15,7 @@ SNAPSHOT_LOOKUP_TAG_VALUE=$2
 MOUNT_PATH=$3
 
 DEVICE=$(lsblk | egrep " $MOUNT_PATH\$" | awk '{ print "/dev/"$1 }')
-VOLUME_ID=$(aws ec2 describe-volumes --output json --query "Volumes[*].Attachments[*]" | jq -r ".[]|.[]|select(.Device==\"$DEVICE\").InstanceId")
+VOLUME_ID=$(aws ec2 describe-volumes --output json --query "Volumes[*].Attachments[*]" | jq -r ".[]|.[]|select(.Device==\"$DEVICE\").VolumeId")
 
 if ! create_snapshot $VOLUME_ID $SNAPSHOT_LOOKUP_TAG_KEY $SNAPSHOT_LOOKUP_TAG_VALUE; then
   fail $ERROR

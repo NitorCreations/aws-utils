c2a1f51b15a7659da79c0f50484eaf5216f1815b
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-01-20 02:33:30 +0200

    Specify sha256

--- usr/bin/generate-dummy-certs.sh
+++ usr/bin/generate-dummy-certs.sh
@@ -6,12 +6,12 @@ mkdir -p /etc/letsencrypt/keys/$1/
 mkdir -p /etc/letsencrypt/live/$1/
 chmod -R 755 /etc/letsencrypt
 # CA cert
-openssl req -x509 -nodes -days 365 -newkey rsa:4096 \
+openssl req -x509 -nodes -days 365 -newkey rsa:4096 -sha256 \
 -keyout /etc/letsencrypt/keys/$1/chainkey-$TSTAMP.pem \
 -out /etc/letsencrypt/keys/$1/chain-$TSTAMP.pem \
 -subj "/C=FI/ST=Uusimaa/L=Helsinki/O=Nitor Creations Oy/OU=IT/CN=$1"
 # CSR
-openssl req -nodes -days 365 -newkey rsa:4096 \
+openssl req -nodes -days 365 -newkey rsa:4096 -sha256 \
 -keyout /etc/letsencrypt/keys/$1/privkey-$TSTAMP.pem \
 -out /etc/letsencrypt/keys/$1/certcsr-$TSTAMP.pem \
 -subj "/C=FI/ST=Uusimaa/L=Helsinki/O=Nitor Creations Oy/OU=IT/CN=$1"

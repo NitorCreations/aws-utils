673b72ac9f732318ff71deffff0dda427b083da1
Jonas Berlin <jonas.berlin@nitorcreations.com>
2016-01-26 13:41:29 +0200

    Add/update licenses for all maintained files

--- opt/nitor/tool_installers.sh
+++ opt/nitor/tool_installers.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# Copyright 2016 Nitor Creations Oy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Functions to install various tools meant to be sourced and used as Functions
 if [ -z "$AWSUTILS_VERSION" ]; then
   AWSUTILS_VERSION=0.9

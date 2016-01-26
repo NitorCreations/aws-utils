a29466b594997bc9aaceaf9d9fa55b69939e6d81
Jonas Berlin <jonas.berlin@nitorcreations.com>
2016-01-26 16:48:35 +0200

    add ec2_associate_address.sh

---
+++ usr/bin/ec2_associate_address.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -e
+
+# Copyright 2016 Nitor Creations Oy
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if [ ! "$CF_paramEipAllocationId" ]; then
+  echo "IP address not associated -- Elastic IP allocation id not configured"
+elif ! aws --region ${CF_AWS__Region} ec2 associate-address --instance-id $INSTANCE_ID --allocation-id ${CF_paramEipAllocationId} --allow-reassociation; then
+  echo "IP address association failed!"
+  exit 1
+fi

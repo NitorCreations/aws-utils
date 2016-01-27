e0cfc261624adea37610b89db2da09191795d5f7
Jonas Berlin <jonas.berlin@nitorcreations.com>
2016-01-27 12:01:18 +0200

    cloud_init_functions: Set INSTANCE_ID only if not already set

--- opt/nitor/cloud_init_functions.sh
+++ opt/nitor/cloud_init_functions.sh
@@ -74,7 +74,7 @@ onexit () {
   aws --region ${CF_AWS__Region} cloudformation signal-resource --stack-name ${CF_AWS__StackName} --logical-resource-id resourceAsg --unique-id $INSTANCE_ID --status $status
 }
 
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+[ "${INSTANCE_ID}" ] || INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 trap onexit EXIT
 status=FAILURE
 

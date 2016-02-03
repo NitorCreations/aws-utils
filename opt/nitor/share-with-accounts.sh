2aac3dfe9b9fce72a28e395cd593f7ab45bf6863
Jonas Berlin <jonas.berlin@nitorcreations.com>
2016-02-03 18:57:28 +0200

    bakery-jobs: Force "bash -xe" in all baking shell scripts

--- opt/nitor/share-with-accounts.sh
+++ opt/nitor/share-with-accounts.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -xe
+
 if [ -z "$1" -o -z "$2" ]; then
   echo "usage: $0 ami-id account-id [account-id ...]"
   exit 1

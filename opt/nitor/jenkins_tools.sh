de374dd80f5ddca08b74b88c8c329d1332e46963
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-03-22 11:07:13 +0200

    Allow mounting EBS without the lambda function to clean snapshots

--- opt/nitor/jenkins_tools.sh
+++ opt/nitor/jenkins_tools.sh
@@ -34,7 +34,7 @@ jenkins_mount_home () {
 }
 
 jenkins_mount_ebs_home () {
-  check_parameters CF_paramEBSTag CF_resourceDeleteSnapshotsLambda
+  check_parameters CF_paramEBSTag
   local SIZE=$1
   if [ -z "$SIZE" ]; then
     SIZE=32
@@ -44,9 +44,11 @@ jenkins_mount_ebs_home () {
   cat > /etc/cron.d/${CF_paramEBSTag}-snapshot << MARKER
 30 * * * * root /usr/bin/snapshot-from-volume.sh -w ${CF_paramEBSTag} ${CF_paramEBSTag} $MOUNT_PATH >> /var/log/snapshots.log 2>&1
 MARKER
-  cat > /etc/cron.d/${CF_paramEBSTag}-clean << MARKER
+  if [ -n "${CF_resourceDeleteSnapshotsLambda}" ]; then
+    cat > /etc/cron.d/${CF_paramEBSTag}-clean << MARKER
 45 4 * * * root /usr/bin/clean-snapshots.sh ${CF_resourceDeleteSnapshotsLambda} >> /var/log/snapshots.log 2>&1
 MARKER
+  fi
 
 }
 

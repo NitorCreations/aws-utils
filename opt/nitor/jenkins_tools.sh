aee16fd2e6dfb090f4eef24c1c4334a95cc868df
Pasi Niemi <pasi.niemi@nitorcreations.com>
2016-04-09 11:34:03 +0300

    An attempt to fix bob deployment

--- opt/nitor/jenkins_tools.sh
+++ opt/nitor/jenkins_tools.sh
@@ -105,6 +105,8 @@ jenkins_fetch_repo () {
       sudo -iu jenkins git --git-dir=/var/lib/jenkins/jenkins-home/.git remote add -f -t master origin ${CF_paramJenkinsGit}
     fi
     echo "Checking out jenkins config git repo ${CF_paramJenkinsGit}"
+    #Either the correct gitignore is created via the checkout, or the default one is added later
+    rm /var/lib/jenkins/jenkins-home/.gitignore
     sudo -iu jenkins git --git-dir=/var/lib/jenkins/jenkins-home/.git --work-tree=/var/lib/jenkins/jenkins-home checkout -f master
   else
     echo "Created EBS backed jenkins config"
